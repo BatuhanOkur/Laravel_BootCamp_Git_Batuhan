0f5a98b5e7f14a4e98b7c18c61d7bcbf2072264d
Batuhan Okur <180401011@ogr.comu.edu.tr>
2020-08-23 10:16:19 +0300

    bootcampProject updated.

--- bootcampProject/resources/views/users/create.blade.php
+++ bootcampProject/resources/views/users/create.blade.php
@@ -10,26 +10,32 @@
 
     <title>Hello, world!</title>
 </head>
-<body class="text-center">
+<body class="text-center" style="background: #F7F7F7;">
+
 
 <div class="input-group" style="margin-left: 550px; margin-top: 200px;">
-    <form class="form-signin">
-        <div class="form-group ">
-            <label for="name" class="float-left h5">Name</label>
-            <input type="text" class="form-control" id="name"  placeholder="Type here your name...">
+    <div class="card">
+        <h5 class="card-header" style="background-color: black; color: white;">Sign Up</h5>
+        <div class="card-body">
+            <form class="form-signin">
+                <div class="form-group ">
+                    <label for="name" class="float-left h5">Name</label>
+                    <input type="text" class="form-control" id="name"  placeholder="Type here your name...">
 
-        </div>
-        <div class="form-group">
-            <label for="email" class="float-left h5">Email address</label>
-            <input type="email" class="form-control" id="email"  placeholder="Type here your email address...">
+                </div>
+                <div class="form-group">
+                    <label for="email" class="float-left h5">Email address</label>
+                    <input type="email" class="form-control" id="email"  placeholder="Type here your email address...">
 
+                </div>
+                <div class="form-group">
+                    <label for="password" class="float-left h5">Password</label>
+                    <input type="password" class="form-control" id="password" placeholder="Type here your password...">
+                </div>
+                <button type="submit" class="btn btn-dark">Submit</button>
+            </form>
         </div>
-        <div class="form-group">
-            <label for="password" class="float-left h5">Password</label>
-            <input type="password" class="form-control" id="password" placeholder="Type here your password...">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    </div>
 </div>
 
 </body>
